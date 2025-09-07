903de07a2266d6c2760dc7e557a5be2a1b88cb62
Mahdiyar <141164728+smmhnp@users.noreply.github.com>
2025-09-07 13:24:08 +0330

    create default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -12,7 +13,22 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        User::factory(5)->create();
-    }
+{
+    User::updateOrCreate(
+        ['email' => 'admin@example.com'],
+        [
+            'firstname' => 'Ali',
+            'lastname' => 'Alavi',
+            'nickname' => 'Admin',
+            'role' => 'super_admin',
+            'unit' => 'dev',
+            'email_hash' => hash('sha256', 'admin@example.com'),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'status' => 'active'
+        ]
+    );
+
+    User::factory(10)->create();
+}
 }
